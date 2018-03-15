4c16e1361dc52715880d19318c83bb5c675982ca
Dr Paparoni <adtune6509@student.waynecc.edu>
2018-03-14 22:35:37 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require("express")();
+
+var server = app.listen(process.env.PORT, function() {
+    console.log("Server on")
+})
+
+var io = require("socket.io")(server);
+var totalPlayers = 0;
+var data = 0;
+io.on('connection', function(client){
+    totalPlayers++
+    console.log(client.id+ ' Connected!')
+    client.on('hi', function() {
+        console.log('hi');
+    })
+    
+    client.on('player-connected', function(){
+        client.emit('online-players', totalPlayers);
+    })
+    
+    client.on('disconnect', function(){
+        totalPlayers--
+    })
+})
